f8b67297784ed2aef56aff4e935b409e7639c1d2
MuneneRubia <ianmunene7890@gmail.com>
2023-09-12 12:17:33 -0700

    Write a function that reads a text file (UTF8) and prints it to stdout:
    Prototype: def read_file(filename=):
    You must use the with statement
    You don’t need to manage file permission or file doesn't exist exceptions.
    You are not allowed to import any module

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text file"""
+
+
+def read_file(filename=""):
+    """Prints UTF8 text file contents to stdout"""
+    with open(filename, encoding="utf-8") as f:
